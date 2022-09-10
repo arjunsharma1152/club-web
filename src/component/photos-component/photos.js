5ebec6bbb7166ffd1dca37d2bd32e39f099d7335
Arjun Sharma <arjunsk923@gmail.com>
2022-09-10 20:37:26 +0000

    Update 7 files

--- src/component/photos-component/photos.js
+++ src/component/photos-component/photos.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./photos.scss";
 
 const Photos = ({ imageUrl }) => (
-  <div className="container">
+  <div className="container1">
     <img src={imageUrl} />
   </div>
 );
--- src/pages/event/eventpage.js
+++ src/pages/event/eventpage.js
@@ -1,9 +1,34 @@
 import React from "react";
+import { db } from "../../component/firebase/firebase";
 
-const EventPage = () => (
-  <div>
-    <h1>Event</h1>
-  </div>
-);
+class EventPage extends React.Component {
+  state = {
+    events: null,
+  };
+  componentDidMount() {
+    db.collection("events")
+      .get()
+      .then((snapshot) => {
+        const events = [];
+        events.forEach((doc) => {
+          const data = doc.data();
+          events.push(data);
+        });
+        this.setState({ events: events });
+      })
+      .catch((err) => console.log(err));
+  }
+
+  render() {
+    return (
+      <div className="events">
+        {this.state.events &&
+          this.state.events.map((event) => {
+            return <div></div>;
+          })}
+      </div>
+    );
+  }
+}
 
 export default EventPage;
