6535ac8b9834422657ca463a70fe221e8b9119c4
Arjun Sharma <arjunsk923@gmail.com>
2022-09-11 13:11:01 +0000

    Update 4 files

--- src/component/firebase/firebase.js
+++ src/component/firebase/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
-import { getDatabase } from "firebase/database";
 import firebase from "firebase/compat/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDdIHQVgvrR41zt9OWeqJlG22aZGlMYzKk",
@@ -15,7 +15,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const db = getDatabase(app);
+export const db = getFirestore(app);
 
 export const storage = getStorage(app);
 
